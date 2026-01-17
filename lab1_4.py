537d0f0fd79a3a70bf33164e1af6c27533a4cb23
joshbuerger <joshybuerger@gmail.com>
2026-01-17 01:00:31 +0000

    Completed Lab 1

--- lab1_4.py
+++ lab1_4.py
@@ -1,1 +1,12 @@
-pass
+def calculate_average(num1: float, num2: float, num3: float):
+    total = num1 + num2 + num3
+    return total / 3
+
+def add_tax(bill_total: float):
+     return bill_total * 1.10
+
+def greet_user(name: str):
+     str = "Hello " + name
+     return str
+
+
